28e7b35db20b8468a5ea7e19a046bbad2618803b
Hugo BD <hugo.belot-deloro@epita.fr>
2019-01-25 12:05:04 +0100

    Test de la physique et controles de base

---
+++ The Maze VR/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("PlayerMovement script loaded");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("z"))
+        {
+            Debug.Log("Go Forwards !");
+            Rb.AddForce(200f * Time.deltaTime,0,0);
+        }
+        
+        if (Input.GetKey("s"))
+        {
+            Rb.AddForce(-200f * Time.deltaTime,0,0);
+        }
+        
+        if (Input.GetKey("q"))
+        {
+            Rb.AddForce(0,0,200f * Time.deltaTime);
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            Rb.AddForce(0,0,-200f * Time.deltaTime);
+        }
+    }
+}
