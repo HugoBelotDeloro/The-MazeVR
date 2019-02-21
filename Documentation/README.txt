47df249d7331a8c1712a46711bd9a8c8c9417095
Asteri0s <46998106+Asteri0s@users.noreply.github.com>
2019-02-21 14:07:48 +0100

    Generateur de labyrinthe
    Generateur de labyrinthe a partir d'image

--- Documentation/README.txt
+++ Documentation/README.txt
@@ -1,1 +1,10 @@
 Ce dossier sert uniquement à la documentation !
+
+pour le generateur de labyrinthe :
+metre une image en parametre Map du Generator, un modele est deja present dans le dossier maps
+pour qu'une image puisse etre utilisée pour creer un labyrinthe, il faut que ses parametres dans unity soient configurés ainsi:
+Filter Mode : Point (no filter)
+Compression : None
+dans Advanced :
+Non Power of 2 : None
+Read/Write Enabled : oui
---
+++ The Maze VR/Assets/script/Generator/Generator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public Texture2D map;
+
+    public ListPrefab[] P;
+
+    public void GenerateLab()
+    {
+        for (int j = 0; j < map.height; j++)
+        {
+            for (int i = 0; i < map.width; i++)
+            {
+                Color c = map.GetPixel(i, j);
+                if (i % 2 == 0 && j % 2 == 0)
+                {
+                    if (c == Color.black)
+                        create("Pillar", i, j);
+                }
+                else if (i % 2 == 1 && j % 2 == 0)
+                {
+                    if (c == Color.black)
+                        create("WallZ", i, j);
+                }
+                else if (i % 2 == 0 && j % 2 == 1)
+                {
+                    if (c == Color.black)
+                        create("WallX", i, j);
+                }
+                else if (i % 2 == 1 && j % 2 == 1)
+                {
+                    create("GroundTile", i, j);
+                }
+            }
+        }
+    }
+
+    public void create(string name, int x,int y){
+        Vector3 v;
+        foreach (ListPrefab G in P)
+            {
+                if (G.name == name)
+                {
+                    switch (G.name)
+                    {
+                        case ("Pillar"):
+                            v = new Vector3(2 * x, (float) 2.5, 2 * y);
+                            Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
+                        case ("WallZ"):
+                            v = new Vector3(2 * x, (float) 2.5, 2 * y);
+                            Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
+                        case ("WallX"):
+                            v = new Vector3(2 * x, (float) 2.5, 2 * y);
+                            Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
+                        case ("GroundTile"):
+                            v = new Vector3(2*x,0,2*y);
+                            Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
+                    }
+                }
+            }
+    }
+    
+    void Start()
+    {
+        GenerateLab();
+    }
+}
