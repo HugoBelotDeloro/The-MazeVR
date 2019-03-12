e0fa5bd7ca7255b166b35bc239844c6f2b0141c9
Asteri0s <46998106+Asteri0s@users.noreply.github.com>
2019-03-12 11:33:32 +0100

    labyrinthe wall
    plus besoin de 2 prefab differents pour les murs du labyrinthe

--- The Maze VR/Assets/script/Generator/Generator.cs
+++ The Maze VR/Assets/script/Generator/Generator.cs
@@ -22,29 +22,29 @@ public class Generator : MonoBehaviour
                 if (i % 2 == 0 && j % 2 == 0)
                 {
                     if (c == Color.black)
-                        create("Pillar", i, j);
+                        create("Pillar", i, j,false);
                 }
                 else if (i % 2 == 1 && j % 2 == 0)
                 {
                     if (c == Color.black)
-                        create("WallZ", i, j);
+                        create("Wall", i, j,true);
                 }
                 else if (i % 2 == 0 && j % 2 == 1)
                 {
                     if (c == Color.black)
-                        create("WallX", i, j);
+                        create("Wall", i, j,false);
                 }
                 else if (i % 2 == 1 && j % 2 == 1)
                 {
                     if (c==Color.red)
-                        create("Player",i,j);
-                    create("GroundTile", i, j);
+                        create("Player",i,j,false);
+                    create("GroundTile", i, j,false);
                 }
             }
         }
     }
 
-    public void create(string name, int x,int y){
+    public void create(string name, int x,int y, bool rotate){
         Vector3 v;
         foreach (ListPrefab G in P)
             {
@@ -56,13 +56,12 @@ public class Generator : MonoBehaviour
                             v = new Vector3(2 * x, (float) 2.5, 2 * y);
                             Instantiate(G.prefab, v, Quaternion.identity, transform);
                             break;
-                        case ("WallZ"):
+                        case ("Wall"):
                             v = new Vector3(2 * x, (float) 2.5, 2 * y);
-                            Instantiate(G.prefab, v, Quaternion.identity, transform);
-                            break;
-                        case ("WallX"):
-                            v = new Vector3(2 * x, (float) 2.5, 2 * y);
-                            Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            if (rotate)
+                                Instantiate(G.prefab, v,Quaternion.Euler(new Vector3(0,90,0)), transform);
+                            else
+                                Instantiate(G.prefab, v, Quaternion.identity, transform);
                             break;
                         case ("GroundTile"):
                             v = new Vector3(2*x,0,2*y);
