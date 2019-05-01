07ef74d6d803b2fdcfabf62de6acf573e9582272
Hugo BD <hugo.belot-deloro@epita.fr>
2019-05-01 17:57:55 +0200

    Added the Torch lamp
    -Added the torch lamp prefab. Similarly to WorldItem, it will be picked up on collision. The associated item goes into the equipment light slot. When picked up it will enable the player's torch light.
    -The torch light is now disabled by default.
    -Picked up equipment directly goes into the corresponding equipment slot.

--- The Maze VR/Assets/Script/Player/Inventory.cs
+++ The Maze VR/Assets/Script/Player/Inventory.cs
@@ -8,6 +8,7 @@ public class Inventory : MonoBehaviour
     private const int MaxItems = 3;
     public Item EmptyItem;
     public Image EmptyImage;
+    public Equipment Equipment;
 
     private void Start()
     {
@@ -20,6 +21,10 @@ public class Inventory : MonoBehaviour
 
     public bool AddItem(Item item)
     {
+        if (item.Type != Equipment.ItemType.Regular)
+        {
+            return Equipment.EquipItem(item);
+        }
         int i = 0;
         while (i < MaxItems && Items[i].Item.Type != Equipment.ItemType.Null)
         {
