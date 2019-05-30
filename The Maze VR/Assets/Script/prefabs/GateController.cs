0436609fffe5e4d23229324af970f1e80eff9e7b
Hugo BD <hugo.belot-deloro@epita.fr>
2019-05-30 17:03:12 +0200

    Gates now open
    -The Southern gates now open if you have the space stone in your inventory.
    -Smooth
    -Beautiful

---
+++ The Maze VR/Assets/Script/prefabs/GateController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+	[SerializeField] private GameObject leftDoor;
+	[SerializeField] private GameObject rightDoor;
+	[SerializeField] private Item keyItem;
+	private int _timer;
+	private Vector3 _leftDoorAxis;
+	private Vector3 _rightDoorAxis;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Inventory inventory = other.GetComponentInChildren<Inventory>();
+			if (inventory.RemoveItem(keyItem))
+			{
+				Rotate();
+			}
+		}
+	}
+
+	private void Start()
+	{
+		_leftDoorAxis = leftDoor.transform.position + new Vector3(0, 0, 3.6f);
+		_rightDoorAxis = rightDoor.transform.position - new Vector3(0, 0, 3.6f);
+	}
+
+	private void Update()
+	{
+		if (_timer > 0)
+		{
+			_timer--;
+			leftDoor.transform.RotateAround(_leftDoorAxis, Vector3.up, -1);
+			rightDoor.transform.RotateAround(_rightDoorAxis, Vector3.up, 1);
+		}
+	}
+
+	private void Rotate()
+	{
+		_timer = 80;
+	}
+}
