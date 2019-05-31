e03994feae8d787d682e768679a048e2b4f94e00
Asteri0s <46998106+Asteri0s@users.noreply.github.com>
2019-05-31 17:12:48 +0200

    generateur adapté aux codes

--- The Maze VR/Assets/script/Generator/Generator.cs
+++ The Maze VR/Assets/script/Generator/Generator.cs
@@ -2,41 +2,57 @@
 
 public class Generator : MonoBehaviour
 {
-    public Texture2D map;
-
     public ListPrefab[] P;
+
+    public string code;
+    
+    private Parser p = new Parser();
+
+    private int[,] map;
 
     public void GenerateLab()
     {
-        for (int j = 0; j < map.height; j++)
+        for (int j = 0; j < map.GetLength(1); j++)
         {
-            for (int i = 0; i < map.width; i++)
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                Color c = map.GetPixel(i, j);
+                int c = map[i,j];
                 if (i % 2 == 0 && j % 2 == 0)
                 {
-                    if (c == new Color(0,0,0,1))
+                    if (c == 1)
                         create("Pillar", i, j,false);
                 }
                 else if (i % 2 == 1 && j % 2 == 0)
                 {
-                    if (c ==new Color(0,0,0,1))
+                    if (c == 2)
                         create("Wall", i, j,true);
-                    else if (c ==new Color(1,1,0,1))
+                    else if (c == 4)
                         create("Door", i, j,true);
+                    else if (c == 6)
+                        create("FalseWall", i, j,true);
+                    else if (c == 7)
+                        create("IlluWall", i, j,true);
+                    else if (c == 8)
+                        create("IlluDoor", i, j,true);
                 }
                 else if (i % 2 == 0 && j % 2 == 1)
                 {
-                    if (c ==new Color(0,0,0,1))
+                    if (c == 2)
                         create("Wall", i, j,false);
-                    else if (c ==new Color(1,1,0,1))
+                    else if (c == 4)
                         create("Door", i, j,false);
+                    else if (c == 6)
+                        create("FalseWall", i, j,false);
+                    else if (c == 7)
+                        create("IlluWall", i, j,false);
+                    else if (c == 8)
+                        create("IlluDoor", i, j,false);
                 }
                 else if (i % 2 == 1 && j % 2 == 1)
                 {
-                    if (c==new Color(1, 0.498039216f, 0,1))
+                    if (c== 5)
                         create("Light",i,j,false);
-                    if (c==new Color(1,0,0,1))
+                    if (c== 3)
                         create("Player",i,j,false);
                     create("GroundTile", i, j,false);
                 }
@@ -44,7 +60,8 @@ public class Generator : MonoBehaviour
         }
     }
 
-    public void create(string name, int x,int y, bool rotate){
+    public void create(string name, int y,int x, bool rotate)
+    {
         Vector3 v;
         foreach (ListPrefab G in P)
             {
@@ -58,7 +75,7 @@ public class Generator : MonoBehaviour
                             break;
                         case ("Wall"):
                             v = new Vector3(2 * x+transform.position.x, (float) 2.5+transform.position.y, 2 * y+transform.position.z);
-                            if (rotate)
+                            if (!rotate)
                                 Instantiate(G.prefab, v,Quaternion.Euler(new Vector3(0,90,0)), transform);
                             else
                                 Instantiate(G.prefab, v, Quaternion.identity, transform);
@@ -73,7 +90,7 @@ public class Generator : MonoBehaviour
                             break;
                         case ("Door"):
                             v = new Vector3(2 * x+transform.position.x, (float) 2.5+transform.position.y, 2 * y+transform.position.z);
-                            if (rotate)
+                            if (!rotate)
                                 Instantiate(G.prefab, v,Quaternion.Euler(new Vector3(0,90,0)), transform);
                             else
                                 Instantiate(G.prefab, v, Quaternion.identity, transform);
@@ -82,6 +99,27 @@ public class Generator : MonoBehaviour
                             v=new Vector3(2*x+transform.position.x,(float)4.5 +transform.position.y,2*y+transform.position.z);
                             Instantiate(G.prefab, v, Quaternion.identity, transform);
                             break;
+                        case ("FalseWall"):
+                            v = new Vector3(2 * x+transform.position.x, (float) 2.5+transform.position.y, 2 * y+transform.position.z);
+                            if (!rotate)
+                                Instantiate(G.prefab, v,Quaternion.Euler(new Vector3(0,90,0)), transform);
+                            else
+                                Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
+                        case ("IlluWall"):
+                            v = new Vector3(2 * x+transform.position.x, (float) 2.5+transform.position.y, 2 * y+transform.position.z);
+                            if (!rotate)
+                                Instantiate(G.prefab, v,Quaternion.Euler(new Vector3(0,90,0)), transform);
+                            else
+                                Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
+                        case ("IlluDoor"):
+                            v = new Vector3(2 * x+transform.position.x, (float) 2.5+transform.position.y, 2 * y+transform.position.z);
+                            if (!rotate)
+                                Instantiate(G.prefab, v,Quaternion.Euler(new Vector3(0,90,0)), transform);
+                            else
+                                Instantiate(G.prefab, v, Quaternion.identity, transform);
+                            break;
                     }
                 }
             }
@@ -89,6 +127,7 @@ public class Generator : MonoBehaviour
     
     void Start()
     {
+        map = p.codeToMap(code);
         GenerateLab();
     }
 }
