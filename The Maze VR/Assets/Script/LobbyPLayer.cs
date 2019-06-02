1975aae73816eac8b9f83507d4cb41a65d8eb0b4
Lucien Leseigle <lucien.leseigle@epita.fr>
2019-06-02 16:14:44 +0200

    lobby menu starts the game

--- The Maze VR/Assets/Script/LobbyPLayer.cs
+++ The Maze VR/Assets/Script/LobbyPLayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LobbyPLayer : MonoBehaviour
@@ -15,5 +16,13 @@ public class LobbyPLayer : MonoBehaviour
     public void StartGame()
     {
         Debug.Log("Startgame " + player);
+        if (GameObject.Find("client").GetComponent<client>().S =="VR")
+        {
+            SceneManager.LoadScene("multi J1", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene("multi J2", LoadSceneMode.Single);
+        }
     }
 }
