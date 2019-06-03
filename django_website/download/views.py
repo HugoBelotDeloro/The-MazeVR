aa54be7f54c3db1462e9507adf0119eef59ac231
Lucien Leseigle <lucien.leseigle@epita.fr>
2019-06-03 16:31:31 +0200

    removed website

--- django_website/download/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.contrib.auth.models import User
-
-def download(request):
-    template = loader.get_template('download.html')
-    context = {'pamount': User.objects.all().count()}
-    return HttpResponse(template.render(context, request))
-# Create your views here.
