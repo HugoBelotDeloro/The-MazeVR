9b0d5c93de0206a2414ef4baa209d608418aed43
Hugo BELOT-DELORO <hugo.belot-deloro@epita.fr>
2019-06-04 00:55:09 +0200

    Go back to main menu after endscreen (multiplayer)

---
+++ The Maze VR/Assets/Script/Menus/GoToMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GoToMainMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetAxis("Echap") > 0)
+        {
+            GameManager.Instance.LoadMainMenu();
+        }
+    }
+}
